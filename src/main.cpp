9204da975e67e62c15f5266c66490d1b44113813
walnaimi <waleedalnaimi117@gmail.com>
2025-02-25 03:36:02 +0200

    updating the rotors

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
 #include <thread>
-#include <chrono>
+
+//ASCII to alpha 90-64
+
+char rotorOneAlpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rotorTwoAlpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rotorThreeAlpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 void print_rotors(int rotor1, int rotor2, int rotor3) {
     std::cout << "3rd\t\t2nd\t\t1st\n";
@@ -10,10 +15,24 @@ void print_rotors(int rotor1, int rotor2, int rotor3) {
 }
 
 bool check_rotor(int rotor) {
-    if (rotor < 0 || rotor > 25) {
+    if (rotor < 1 || rotor > 26) {
         return false;
     }
     return true;
+}
+
+std::string encrypt_message(const std::string &message, int &rotor1, int &rotor2, int &rotor3){
+    std::string encrypted_message = message;
+    for (char& c : encrypted_message) {
+        if (c >= 'A' && c <= 'Z') {
+            c = rotor1;
+            if rotor
+            c = rotor2;
+            c = rotor3:
+        }
+    }
+    return encrypted_message;
+
 }
 
 int main() {
@@ -28,7 +47,7 @@ int main() {
         // 1st rotor input with text animation
         int rotor1, rotor2, rotor3;
         while(true) {
-        std::cout << "Input rotors (0-25): [3rd] [2nd] [1st]\n";
+        std::cout << "Input rotors (1-26): [3rd] [2nd] [1st]\n";
         std::getline(std::cin, input);
         std::sscanf(input.c_str(), "%d %d %d", &rotor3, &rotor2, &rotor1);
         if (!check_rotor(rotor1) || !check_rotor(rotor2) || !check_rotor(rotor3)) {
@@ -39,6 +58,10 @@ int main() {
     }
         // Print the final updated rotors without creating a new line
         print_rotors(rotor1, rotor2, rotor3);
+
+        std::string encrypted_message = encrypt_message(message, rotor1, rotor2, rotor3);
+        std::cout << "rotors: " << rotor1 << " " << rotor2 << " " << rotor3 << std::endl;
+        std::cout << "Encrypted message: " << encrypted_message << std::endl;
         break;  // Exit the loop once all inputs are valid
     }
 
